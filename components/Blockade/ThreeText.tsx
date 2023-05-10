c4085ce9f15f159b7f61d233c30a1f57a1d3e805
colinmfoster4723 <colin.foster4723@gmail.com>
2023-05-09 20:08:06 -0500

    Added type for 3d text

--- components/Blockade/ThreeText.tsx
+++ components/Blockade/ThreeText.tsx
@@ -2,12 +2,20 @@ import { extend } from "@react-three/fiber";
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
 import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
 import goodtimes from "../../public/fonts/goodtimes.json";
+import { Object3DNode } from "@react-three/fiber";
+
+class Text extends TextGeometry {}
 
 extend({ TextGeometry });
 
-export default function ThreeText({ position, text }: any) {
-  const font = new FontLoader().parse(goodtimes);
+declare module "@react-three/fiber" {
+  interface ThreeElements {
+    textGeometry: Object3DNode<Text, typeof Text>;
+  }
+}
 
+export default function ThreeText({ position, text }: any) {
+  const font: any = new FontLoader().parse(goodtimes);
   return (
     <mesh position={position}>
       <textGeometry args={[text, { font, size: 1, height: 1 }]} />
