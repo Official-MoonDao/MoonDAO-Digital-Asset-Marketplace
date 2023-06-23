21828da73ac8e524538dafa18646e2bec4797ae3
colinmfoster4723 <colin.foster4723@gmail.com>
2023-06-23 00:35:16 -0500

    Adjusted hero image skeleton for mobile

--- webapp/components/Home/HeroImage.tsx
+++ webapp/components/Home/HeroImage.tsx
@@ -8,7 +8,7 @@ export default function HeroImage({ asset }: any) {
   if (!nft)
     return (
       <div
-        className={`bg-gradient-to-r from-[#333] via-[#555] to-[#333] bg-[length:400px_400px] animate-pulse duration-[1s] ease-in-out infinite p-2 m-2  h-[362px] lg:h-[443.38px] xl:h-[499.58px] 2xl:h-[564px]  object-cover lg:w-[355px] xl:w-[400px] 2xl:w-[536px]  rounded-tl-[99px] rounded-br-[99px]`}
+        className={`w-[290px] bg-gradient-to-r from-[#333] via-[#555] to-[#333] bg-[length:400px_400px] animate-pulse duration-[1s] ease-in-out infinite p-2 m-2  h-[362px] lg:h-[443.38px] xl:h-[499.58px] 2xl:h-[564px]  object-cover lg:w-[355px] xl:w-[400px] 2xl:w-[536px]  rounded-tl-[99px] rounded-br-[99px]`}
       />
     );
 
