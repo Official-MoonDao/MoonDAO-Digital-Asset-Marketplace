1ee66698b07eab93cc34180cfcd9b99533e2d7be
colinmfoster4723 <colin.foster4723@gmail.com>
2023-06-29 14:55:17 -0500

    Adjusted network and null nft check for sell page

--- webapp/lib/marketplace/hooks/useUserAssets.tsx
+++ webapp/lib/marketplace/hooks/useUserAssets.tsx
@@ -25,83 +25,88 @@ export function useUserAssets(
     listings: profileListings,
     auctions: profileAuctions,
     isLoading: loadingProfileAuctions,
-  } = useListingsByWallet(validListings, validAuctions, walletAddress);
+  } = useListingsByWallet(validListings, validAuctions, signer?._address);
 
   useEffect(() => {
     if (
       marketplace &&
-      signer &&
       profileListings &&
+      signer &&
       profileAuctions &&
       !networkMismatch
     ) {
       if (loadingProfileAuctions) return;
-      setAssets([]);
       marketplace.roles.get("asset").then(async (res: any) => {
+        setAssets([]);
         await res.forEach(async (collection: any) => {
           if (networkMismatch) return;
-          const sdk: ThirdwebSDK = ThirdwebSDK.fromSigner(signer, NETWORK);
-          const contract: any = await sdk.getContract(collection);
-          const extensions = getAllDetectedExtensionNames(contract.abi);
-          let ownedAssets: any;
-          if (extensions[0] === "ERC1155") {
-            ownedAssets = await contract.erc1155.getOwned(walletAddress);
-            //Create a new array of ownedAssets with quantityOwned updated to reflect the number of assets not listed on the marketplace
-            ownedAssets = await ownedAssets.map((asset: any) => {
-              const ownedQuantity = asset.quantityOwned;
+          try {
+            const sdk: ThirdwebSDK = ThirdwebSDK.fromSigner(signer, NETWORK);
+            const contract: any = await sdk.getContract(collection);
+            const extensions = getAllDetectedExtensionNames(contract.abi);
+            let ownedAssets: any;
+            if (extensions[0] === "ERC1155") {
+              ownedAssets = await contract.erc1155.getOwned(signer._address);
+              //Create a new array of ownedAssets with quantityOwned updated to reflect the number of assets not listed on the marketplace
+              ownedAssets = await ownedAssets.map((asset: any) => {
+                if (asset.status === 4) return;
+                const ownedQuantity = asset.quantityOwned;
 
-              //only count direct listings, auction listings are automatically subtracted from asset.quantityOwned
+                //only count direct listings, auction listings are automatically subtracted from asset.quantityOwned
 
-              const listedQuantity = profileListings?.reduce(
-                (arr: number, listing: any) =>
-                  listing.assetContractAddress.toLowerCase() ===
-                    collection.toLowerCase() &&
-                  listing.tokenId === asset.metadata.id
-                    ? arr + Number(listing?.quantity)
-                    : arr,
-                0
-              );
+                const listedQuantity = profileListings?.reduce(
+                  (arr: number, listing: any) =>
+                    listing.assetContractAddress.toLowerCase() ===
+                      collection.toLowerCase() &&
+                    listing.tokenId === asset.metadata.id
+                      ? arr + Number(listing?.quantity)
+                      : arr,
+                  0
+                );
 
-              return {
-                ...asset,
-                quantityOwned: ownedQuantity - listedQuantity,
-              };
-            });
-          } else {
-            ownedAssets = await contract.erc721.getOwned(walletAddress);
+                return {
+                  ...asset,
+                  quantityOwned: ownedQuantity - listedQuantity,
+                };
+              });
+            } else {
+              ownedAssets = await contract.erc721.getOwned(signer._address);
 
-            ownedAssets = ownedAssets
-              .filter(
-                (asset: any) =>
-                  !profileListings?.find(
-                    (listing: any) =>
-                      listing.assetContractAddress === collection &&
-                      listing.tokenId === asset.metadata.id
-                  ) &&
-                  !profileAuctions?.find(
-                    (auction: any) =>
-                      auction.assetContractAddress === collection &&
-                      auction.tokenId === asset.metadata.id
-                  )
-              )
-              .map((asset: any) => ({ ...asset, quantityOwned: "1" }));
+              ownedAssets = ownedAssets
+                .filter(
+                  (asset: any) =>
+                    !profileListings?.find(
+                      (listing: any) =>
+                        listing.assetContractAddress === collection &&
+                        listing.tokenId === asset.metadata.id
+                    ) &&
+                    !profileAuctions?.find(
+                      (auction: any) =>
+                        auction.assetContractAddress === collection &&
+                        auction.tokenId === asset.metadata.id
+                    )
+                )
+                .map((asset: any) => ({ ...asset, quantityOwned: "1" }));
+            }
+
+            const collectionName = await contract.call("name");
+
+            //add collection data to ownedAssets
+            ownedAssets = await ownedAssets.map((asset: any) => ({
+              ...asset,
+              collection,
+              collectionName,
+            }));
+
+            //add ownedAssets to assets array and filter out any duplicates (on address change duplicates are created and then filtered out, this is a quick fix)
+            ownedAssets.length > 0 &&
+              setAssets((prev: any) => [
+                ...prev.filter((a: any) => a.collection !== collection),
+                ...ownedAssets,
+              ]);
+          } catch (err: any) {
+            console.log(err.message);
           }
-
-          const collectionName = await contract.call("name");
-
-          //add collection data to ownedAssets
-          ownedAssets = await ownedAssets.map((asset: any) => ({
-            ...asset,
-            collection,
-            collectionName,
-          }));
-
-          //add ownedAssets to assets array and filter out any duplicates (on address change duplicates are created and then filtered out, this is a quick fix)
-          ownedAssets.length > 0 &&
-            setAssets((prev: any) => [
-              ...prev.filter((a: any) => a.collection !== collection),
-              ...ownedAssets,
-            ]);
         });
       });
     }
