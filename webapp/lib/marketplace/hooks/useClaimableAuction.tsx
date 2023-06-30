1c3e1584364aad41a28a56f7a7f78a92c58fc621
colinmfoster4723 <colin.foster4723@gmail.com>
2023-06-30 12:40:41 -0500

    Adjusted winningBid obj

--- webapp/lib/marketplace/hooks/useClaimableAuction.tsx
+++ webapp/lib/marketplace/hooks/useClaimableAuction.tsx
@@ -3,18 +3,17 @@ import { MOONEY_DECIMALS, ZERO_ADDRESS } from "../../../const/config";
 
 //Check if closed auction has a payout
 export function useClaimableAuction(
-  winningBid: number,
+  winningBidObj: any,
   buyoutBidAmount: number,
   endTimestamp: string | number
 ) {
   const claimable = useMemo(() => {
-    console.log(winningBid, buyoutBidAmount, endTimestamp);
     const now = Date.now() / 1000;
-    if (winningBid?.bidderAddress === ZERO_ADDRESS) return false;
+    if (winningBidObj?.bidderAddress === ZERO_ADDRESS) return false;
     return (
-      winningBid >= +buyoutBidAmount / MOONEY_DECIMALS ||
-      (winningBid > 0 && +endTimestamp < now)
+      winningBidObj.bidAmount >= +buyoutBidAmount ||
+      (winningBidObj.bidAmount > 0 && +endTimestamp < now)
     );
-  }, [winningBid, buyoutBidAmount]);
+  }, [winningBidObj, buyoutBidAmount]);
   return claimable;
 }
