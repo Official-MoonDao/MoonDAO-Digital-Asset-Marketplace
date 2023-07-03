22f9b76bfffaa71c035dd4e2cf1fafa8eee54ceb
colinmfoster4723 <colin.foster4723@gmail.com>
2023-07-03 14:47:08 -0500

    Added thirdwebcdn domain to nextconfig images

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: "",
   images: {
-    domains: ["ipfs-3.thirdwebcdn.com", "i.seadn.io", "ipfs.io"],
+    domains: [
+      "ipfs-3.thirdwebcdn.com",
+      "ipfs-public.thirdwebcdn.com",
+      "i.seadn.io",
+      "ipfs.io",
+    ],
   },
 
   webpack: (config) => {
