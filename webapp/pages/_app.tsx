6471821c87e2046f6c3d0bab6bb837161c36838f
colinmfoster4723 <colin.foster4723@gmail.com>
2023-10-19 21:20:45 -0500

    Fixed app props type error

--- webapp/pages/_app.tsx
+++ webapp/pages/_app.tsx
@@ -1,6 +1,4 @@
-import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
-import NextNProgress from "nextjs-progressbar";
 import "../styles/globals.css";
 import Layout from "../components/Layout/Layout";
 import { NETWORK } from "../const/config";
@@ -10,7 +8,8 @@ import {
   safeWallet,
   walletConnect,
 } from "@thirdweb-dev/react";
-function MyApp({ Component, pageProps }: AppProps) {
+
+function MyApp({ Component, pageProps }: any) {
   return (
     <ThirdwebProvider
       clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}
@@ -22,14 +21,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         walletConnect(),
       ]}
     >
-      {/* Progress bar when navigating between pages */}
-      <NextNProgress
-        color="var(--color-tertiary)"
-        startPosition={0.3}
-        stopDelayMs={200}
-        height={3}
-        showOnShallow={true}
-      />
       <Layout>
         <Component {...pageProps} />
       </Layout>
